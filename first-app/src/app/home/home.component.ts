65b41e172053b40a2b98ef89dd1c1687117a1348
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2024-01-11 00:17:06 +0200

    Create Location component

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    HousingLocationComponent
   ],
   template: `
     <section>
@@ -14,7 +15,10 @@ import { CommonModule } from '@angular/common';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
-  `,
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
+    `,
   styleUrls: ['./home.component.css'],
 })
 
---
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+
+export class HousingLocationComponent {
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
