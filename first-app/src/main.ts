c144a0f2c4901b4f88a36b83184062e00f38b374
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2024-01-10 23:56:06 +0200

    Create Home component

--- first-app/src/main.ts
+++ first-app/src/main.ts
@@ -1,6 +1,17 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+/*
+*  Protractor support is deprecated in Angular.
+*  Protractor is used in this example for compatibility with Angular documentation tools.
+*/
+import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent,
+    {providers: [provideProtractorTestingSupport()]})
+  .catch(err => console.error(err));
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
