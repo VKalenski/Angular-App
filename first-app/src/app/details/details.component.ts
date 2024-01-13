1739443e4c10b160e4d72715ceb03df9d62c6c1b
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2024-01-13 15:48:20 +0200

    Refactoring to show properties

--- first-app/src/app/details/details.component.ts
+++ first-app/src/app/details/details.component.ts
@@ -60,9 +60,7 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
-    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
-      this.housingLocation = housingLocation;
-    });
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
 
   submitApplication() {
@@ -72,5 +70,4 @@ export class DetailsComponent {
       this.applyForm.value.email ?? ''
     );
   }
-
 }
--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -26,3 +26,10 @@ export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
 
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
